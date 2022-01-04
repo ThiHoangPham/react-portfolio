59c01e7e723f806f8370ee67383ac7d1eecd7c5d
Adam Pham <thaihoangpham2008@gmail.com>
2022-01-04 16:45:10 +0800

    created global.scss, working on topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,7 +3,9 @@ import "./topbar.scss"
 export default function Topbar() {
     return (
         <div className="topbar">
+            <div className="wrapper">
 
+            </div>
         </div>
     )
 }
