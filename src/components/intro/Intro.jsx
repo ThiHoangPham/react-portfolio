01dbe0f088378803f6edf47a5569e4abc825af61
Adam Pham <thaihoangpham2008@gmail.com>
2022-01-11 18:10:32 +0800

    almost finish intro page, working on typing animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -8,7 +8,16 @@ export default function Intro() {
                     <img src="assets/man.png" alt="adam pham photo" />
                 </div>
             </div>
-            <div className="right"></div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Adam Pham</h1>
+                    <h3>Web Developer<span></span></h3>
+                </div>
+                <a href="#potfolio">
+                    <img src="assets/down.png" alt="down arrow" />
+                </a>
+            </div>
         </div>
     )
 }
