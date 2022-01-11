f74b3b7ace2c10cb2d685d49719dc2d81460fe0a
Adam Pham <thaihoangpham2008@gmail.com>
2022-01-11 13:05:26 +0800

    working on intro.jsx

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,12 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt="adam pham photo" />
+                </div>
+            </div>
+            <div className="right"></div>
         </div>
     )
 }
