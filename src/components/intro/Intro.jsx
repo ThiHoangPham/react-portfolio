029374f29d2ec962b3abcf3a8f5eff4f601daa4d
Adam Pham <thaihoangpham2008@gmail.com>
2022-01-13 18:29:00 +0800

    portfolio.jsx, added dynamic active class

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,19 @@
 import "./intro.scss"
+import { init } from "ityped"
+import { useEffect, useRef } from "react"
 
 export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: ["Developer", "Designer"],
+        });
+    }, []);
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -12,9 +25,9 @@ export default function Intro() {
                 <div className="wrapper">
                     <h2>Hi There, I'm</h2>
                     <h1>Adam Pham</h1>
-                    <h3>Web Developer<span></span></h3>
+                    <h3>Web <span ref={textRef}></span></h3>
                 </div>
-                <a href="#potfolio">
+                <a href="#portfolio">
                     <img src="assets/down.png" alt="down arrow" />
                 </a>
             </div>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,44 @@
+
+
 import "./portfolio.scss"
 
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-dsadasdas
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>Desing</li>
+                <li>Branding</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="assets/projects/code-quiz-app.jpg" alt="code quiz img" />
+                    <h3>Code Quiz App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/projects/code-quiz-app.jpg" alt="code quiz img" />
+                    <h3>Code Quiz App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/projects/code-quiz-app.jpg" alt="code quiz img" />
+                    <h3>Code Quiz App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/projects/code-quiz-app.jpg" alt="code quiz img" />
+                    <h3>Code Quiz App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/projects/code-quiz-app.jpg" alt="code quiz img" />
+                    <h3>Code Quiz App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/projects/code-quiz-app.jpg" alt="code quiz img" />
+                    <h3>Code Quiz App</h3>
+                </div>
+            </div>
         </div>
     )
 }
