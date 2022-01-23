f55ae6fd0e118376e606b6468dd120ba084bbefc
Adam Pham <thaihoangpham2008@gmail.com>
2022-01-23 09:56:39 +0800

    working on works.jsx

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,7 +3,14 @@ import "./works.scss"
 export default function Works() {
     return (
         <div className="works" id="works">
-
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left"></div>
+                        <div className="right"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
