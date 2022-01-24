7aba3f65c60fe6a5e16d4ff00be7c5139012dd75
Adam Pham <thaihoangpham2008@gmail.com>
2022-01-24 08:49:10 +0800

    coding slider on work.jsx

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -6,8 +6,20 @@ export default function Works() {
             <div className="slider">
                 <div className="container">
                     <div className="item">
-                        <div className="left"></div>
-                        <div className="right"></div>
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src="assets/mobile.png" alt="" />
+                                </div>
+                                <h2>Title</h2>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                                Consequuntur velit sunt error iure quia repellendus tenetur unde fugiat.</p>
+                                <span>learn more...</span>
+                            </div>
+                        </div>
+                        <div className="right">
+                            <img src="https://i.pinimg.com/originals/f6/28/b7/f628b79ea9ba959fcd5196a21df95497.jpg" alt="" />
+                        </div>
                     </div>
                 </div>
             </div>
